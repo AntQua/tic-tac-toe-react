88c8df58e088dce5f27ae0354fcd577658619575
Antonio Quaresma <tozequa@gmail.com>
2024-03-02 17:33:04 +0000

    Update state on Edit button

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,17 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    // Best practice: Updating state based on old state - always use a function
+    // just setIsEditing(!isEditing) is not enough because react just schedules a state update to true
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  // let btnCaption = 'Edit';
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    // btnCaption = 'Save';
   }
 
   return (
@@ -19,7 +23,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
